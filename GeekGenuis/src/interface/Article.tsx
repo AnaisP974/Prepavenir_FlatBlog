7f22c30d9714c53b57567c369d988e389d8f9119
larenta4ks <bah.ibrahima.pro@hotmail.com>
2024-07-08 14:51:16 +0200

    Home page et recuperation affichage des donnees dans la home page

---
+++ GeekGenuis/src/interface/Article.tsx
@@ -0,0 +1,21 @@
+export interface Article {
+
+    id: number,
+
+    titre: string,
+
+    desc: string,
+
+    contenu: string,
+
+    createdAt: string,
+
+    updatedAt: string,
+
+    signature: string,
+
+    category: string,
+
+    img: string,
+
+}
